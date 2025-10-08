daf391e7086422617a45a767e194c0cb98121d32
matheusf21 <matheusferreiradelima21@gmail.com>
2025-10-08 13:59:29 -0300

    V0.1

---
+++ TMovies/navigation/appNavigation.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function appNavigation() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen 
+            name='Home'
+            options={{headerShown: false}}
+            component={HomeScreen}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
